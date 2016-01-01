15baee8933850b39e1ac6464f139d3bbd59eb19e
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-01 11:29:42 -0300

    Delete some code

--- src/model/db.lisp
+++ src/model/db.lisp
@@ -1,25 +1,8 @@
 (in-package :cl-user)
 (defpackage antimer.model.db
   (:use :cl)
-  (:export :database
-           :create-database
-           :connect
-           :disconnect
-           :*db*)
+  (:export :*db*)
   (:documentation "Generic database interface."))
 (in-package :antimer.model.db)
-
-(defclass database ()
-  ()
-  (:documentation "The base class of Antimer databases."))
-
-(defgeneric create-database (database)
-  (:documentation "Create the database."))
-
-(defgeneric connect (database)
-  (:documentation "Connect to the database."))
-
-(defgeneric disconnect (database)
-  (:documentation "Disconnect from the database."))
 
 (defvar *db*)
