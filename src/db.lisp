6ff276b147f7fd7bb4ef2039c161703292c8abdd
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-09 18:55:23 -0300

    Add the source of the database plugin

---
+++ src/db.lisp
@@ -0,0 +1,86 @@
+(in-package :cl-user)
+(defpackage antimer.db
+  (:use :cl)
+  (:import-from :antimer.plugin
+                :plugin
+                :name
+                :short-description
+                :data-directory
+                :on-event)
+  (:import-from :antimer.event
+                :startup
+                :shutdown)
+  (:import-from :crane
+                :make-session
+                :deftable
+                :text)
+  (:export :database
+           :user
+           :make-user
+           :user-username
+           :user-email
+           :user-password)
+  (:documentation "Antimer's relational database interface."))
+(in-package :antimer.db)
+
+;;; Plugin definition
+
+(defclass database ()
+  ((session :accessor plugin-session
+            :type crane:session
+            :documentation "The Crane session object."))
+  (:default-initargs
+   :directory #p"db/")
+  (:documentation "The @c(database) plugin provides an SQL database for Antimer."))
+
+(defmethod name ((plugin database))
+  "SQL Database")
+
+(defmethod short-description ((plugin database))
+  "The database plugin provides an SQL database for storing Antimer's data.")
+
+;;; Tables
+
+(deftable user ()
+  ((username :reader user-username
+             :initarg :username
+             :type text
+             :documentation "The user's username.")
+   (email :reader user-email
+          :initarg :email
+          :type text
+          :documentation "The user's email address.")
+   (password :reader user-password
+             :initarg :password
+             :type text
+             :documentation "The user's hashed password."))
+  (:documentation "Represents a user."))
+
+(defun make-user (username &key email plaintext-password)
+  "Create a user instance. Hash the password in the process."
+  (make-instance 'user
+                 :username username
+                 :email email
+                 :password (cl-pass:hash plaintext-password
+                                         :type :pbkdf2-sha256
+                                         :iterations 200000)))
+
+;;; Events
+
+(defmethod on-event ((plugin database) (event startup))
+  "On startup, create the session object, hook everything up, and start the
+connection."
+  (let ((session (crane:make-session :migratep nil
+                                     :defaultp t)))
+    (crane:define-sqlite3-database antimer-db
+      :name (namestring
+             (merge-pathnames #p"db.sqlite3"
+                              (data-directory plugin))))
+    (crane:register-database session 'antimer-db)
+    (crane:register-table session 'user 'antimer-db)
+    (crane:start session)
+    (setf (plugin-session plugin) session)))
+
+(defmethod on-event ((plugin database) (event shutdown))
+  "On shutdown, cut the connections."
+  (crane:stop (plugin-session plugin)))
