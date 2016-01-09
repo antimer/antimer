0e8cae094083d11cf03e97829d18f96dfc39576d
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-09 17:32:06 -0300

    Delete a couple of old files

--- src/model/db.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :cl-user)
-(defpackage antimer.model.db
-  (:use :cl)
-  (:export :*db*)
-  (:documentation "Generic database interface."))
-(in-package :antimer.model.db)
-
-(defvar *db*)
