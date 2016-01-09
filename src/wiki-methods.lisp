4664f070b4d127f05476a6d0db66d708fc94cd0c
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-09 20:44:04 -0300

    Send the startup method

--- src/wiki-methods.lisp
+++ src/wiki-methods.lisp
@@ -5,6 +5,7 @@
   (let ((port 8000)
         (*wiki* wiki))
     (format t "Starting wiki on port ~D" port)
+    (send (make-instance 'antimer.event:startup))
     ;(lucerne:start antimer.app:app :port port)
     (format t "Server started~%")
     (loop (read))))
