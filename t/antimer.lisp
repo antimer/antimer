db7e109e4db1dc033bbc2897b6abc50a7f8d0a11
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-10 13:08:47 -0300

    Add tests

--- t/antimer.lisp
+++ t/antimer.lisp
@@ -4,5 +4,30 @@
   (:export :run-tests))
 (in-package :antimer-test)
 
+(defparameter +wiki-dir+
+  (asdf:system-relative-pathname :antimer #p"t/wiki/"))
+
+(def-suite database
+  :description "Database tests.")
+(in-suite database)
+
+(test user
+   (let ((user (antimer.db:create-user "test"
+                                       :email "test@gmail.com"
+                                       :plaintext-password "test")))
+     (is
+      (typep user 'antimer.db:user))
+     (is
+      (typep (antimer.db:find-user "test") 'antimer.db:user))
+     (is
+      (null (antimer.db:find-user "nonexistent")))))
+
 (defun run-tests ()
-  t)
+  (ensure-directories-exist +wiki-dir+)
+  (let ((wiki (make-instance 'antimer.wiki:wiki
+                             :directory +wiki-dir+
+                             :plugins (list
+                                       (make-instance 'antimer.db:database)))))
+    (antimer.wiki:start wiki)
+    (run! 'database))
+  (uiop:delete-directory-tree +wiki-dir+ :validate t))
