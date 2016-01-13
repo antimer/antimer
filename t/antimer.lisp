9736112c6ad811f88a1441ee622a65ea68505239
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-13 20:19:14 -0300

    Add some tests

--- t/antimer.lisp
+++ t/antimer.lisp
@@ -7,6 +7,8 @@
 (defparameter +wiki-dir+
   (asdf:system-relative-pathname :antimer #p"t/wiki/"))
 
+;;; Tests
+
 (def-suite database
   :description "Database tests.")
 (in-suite database)
@@ -26,13 +28,71 @@
     (is-false
      (antimer.db:check-password user "test2"))))
 
+(test article
+  (let* ((user (antimer.db:find-user "test"))
+         (article))
+    (is
+     (equal (antimer.db:number-of-articles) 0))
+    (finishes
+     (setf article
+           (antimer.db:create-article "My Article"
+                                      "my-article"
+                                      "This is the text of the article."
+                                      user)))
+    (is
+     (equal (antimer.db:number-of-articles) 1))
+    (is
+     (null (antimer.db:find-article "nonexistent-slug")))
+    (let ((found (antimer.db:find-article "my-article")))
+      (is
+       (string= (antimer.db:article-title found)
+                "My Article")))
+    ;; Editing the article
+    (finishes
+     (antimer.db:edit-article article
+                              "Updated text."
+                              "Changed some things."
+                              user))
+    (let ((found (antimer.db:find-article "my-article")))
+      (is
+       (string= (antimer.db:article-title found)
+                "My Article"))
+      (is
+       (string= (antimer.db:article-source found)
+                "Updated text.")))
+    ;; Edit it again
+    (finishes
+     (antimer.db:edit-article article
+                              "New text."
+                              "More changes."
+                              user))
+    ;; Verify changes
+    (let ((change-count 0))
+      (antimer.db:do-changes (change article)
+        (cond
+          ((= change-count 0)
+           ;; Last change
+           (is
+            (string= (antimer.db:change-message change)
+                     "More changes.")))
+          ((= change-count 1)
+           ;; Change before the last one
+           (is
+            (string= (antimer.db:change-message change)
+                     "Changed some things."))))
+        (incf change-count))
+      (is
+       (equal change-count 3)))))
+
 (defun run-tests ()
-  (ensure-directories-exist +wiki-dir+)
-  (let ((wiki (make-instance 'antimer.wiki:wiki
-                             :directory +wiki-dir+
-                             :plugins (list
-                                       (make-instance 'antimer.db:database)))))
-    (antimer.wiki:start wiki)
-    (run! 'database)
-    (antimer.wiki:stop wiki))
-  (uiop:delete-directory-tree +wiki-dir+ :validate t))
+  (unwind-protect
+       (progn
+         (ensure-directories-exist +wiki-dir+)
+         (let ((wiki (make-instance 'antimer.wiki:wiki
+                                    :directory +wiki-dir+
+                                    :plugins (list
+                                              (make-instance 'antimer.db:database)))))
+           (antimer.wiki:start wiki)
+           (run! 'database)
+           (antimer.wiki:stop wiki)))
+    (uiop:delete-directory-tree +wiki-dir+ :validate t)))
