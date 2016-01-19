e0a752f16dce9db87663726d0a7cf10f56136795
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-18 22:59:31 -0300

    Add views to edit an article and view changes

--- src/core/web.lisp
+++ src/core/web.lisp
@@ -1,6 +1,10 @@
 (in-package :cl-user)
 (defpackage antimer.web
   (:use :cl :lucerne)
+  (:import-from :antimer.wiki
+                :*wiki*
+                :wiki-name
+                :wiki-static-directory)
   (:import-from :antimer.plugin
                 :plugin
                 :name
@@ -18,10 +22,7 @@
 ;;; Plugin definition
 
 (defapp app
-  :middlewares (clack.middleware.session:<clack-middleware-session>
-                (clack.middleware.static:<clack-middleware-static>
-                 :root (asdf:system-relative-pathname :antimer #p"assets/")
-                 :path "/static/")))
+  :middlewares (clack.middleware.session:<clack-middleware-session>))
 
 (defclass web-app (plugin)
   ((port :reader plugin-port
@@ -42,16 +43,20 @@
  (asdf:system-relative-pathname :antimer #p"templates/"))
 
 (defparameter +index+ (djula:compile-template* "index.html"))
+(defparameter +error+ (djula:compile-template* "error.html"))
 (defparameter +register+ (djula:compile-template* "auth/register.html"))
 (defparameter +login+ (djula:compile-template* "auth/login.html"))
 (defparameter +new-article+ (djula:compile-template* "article/new.html"))
 (defparameter +article-list+ (djula:compile-template* "article/list.html"))
 (defparameter +view-article+ (djula:compile-template* "article/view.html"))
+(defparameter +edit-article+ (djula:compile-template* "article/edit.html"))
+(defparameter +article-changes+ (djula:compile-template* "article/changes.html"))
 
 ;;; Views
 
 (defmacro render-view (template &rest arguments)
   `(render-template (,template)
+                    :wiki-name (wiki-name *wiki*)
                     :user (lucerne-auth:get-userid)
                     ,@arguments))
 
@@ -64,12 +69,14 @@
 @route app (:get "/article/new")
 (defview get-new-article ()
   (if (lucerne-auth:logged-in-p)
-      (render-view +new-article+)
       (render-view +new-article+
+                   :title "New Article")
+      (render-view +new-article+
+                   :title "New Article"
                    :error "You must be logged in to create an article.")))
 
 @route app (:post "/article/new")
-(defview get-new-article ()
+(defview post-new-article ()
   (if (lucerne-auth:logged-in-p)
       (flet ((render-error (message)
                (render-view +new-article+
@@ -85,17 +92,21 @@
             (t
              ;; Validate everything
              (let ((user (antimer.db:find-user (lucerne-auth:get-userid))))
-               (antimer.db:create-article title
-                                          slug
-                                          source
-                                          user))
-             (redirect (format nil "/article/~A" slug))))))
+               (if user
+                   (progn
+                     (antimer.db:create-article title
+                                                slug
+                                                source
+                                                user)
+                     (redirect (format nil "/article/~A" slug)))
+                   (render-error "Bad username.")))))))
       (render-view +new-article+
                    :error "You must be logged in to create an article.")))
 
 @route app (:get "/article/all")
 (defview all-articles ()
   (render-view +article-list+
+               :title "All Articles"
                :articles
                (let ((list (list)))
                  (antimer.db:do-articles (article)
@@ -111,10 +122,71 @@
       (if article
           (render-view +view-article+
                        :title (antimer.db:article-title article)
+                       :slug slug
                        :html (antimer.doc:render-document
                               (antimer.doc:parse-document
                                (antimer.db:article-source article))))
           (render-error "No such article.")))))
+
+@route app (:get "/article/:slug/edit")
+(defview get-edit-article (slug)
+  (flet ((render-error (message)
+           (render-view +edit-article+
+                        :error message)))
+    (let ((article (antimer.db:find-article slug)))
+      (if article
+          (if (lucerne-auth:logged-in-p)
+              (render-view +edit-article+
+                           :title (antimer.db:article-title article)
+                           :slug slug
+                           :source (antimer.db:article-source article))
+              (render-view +error+
+                           :message "You need to be logged in to edit an article."))
+          (render-error "No such article.")))))
+
+@route app (:post "/article/:slug/edit")
+(defview post-edit-article (slug)
+  (if (lucerne-auth:logged-in-p)
+      (flet ((render-error (message)
+               (render-view +edit-article+
+                            :error message)))
+        (with-params (source message)
+          (cond
+            ((null source)
+             (render-error "Can't create an empty article"))
+            ((null message)
+             (render-view +edit-article+
+                          :error "You must write a description of the changes."
+                          :slug slug
+                          :source source))
+            (t
+             ;; Validate everything
+             (let ((user (antimer.db:find-user (lucerne-auth:get-userid)))
+                   (article (antimer.db:find-article slug)))
+               (if (and user article)
+                   (progn
+                     (antimer.db:edit-article article
+                                              source
+                                              message
+                                              user)
+                     (redirect (format nil "/article/~A" slug)))
+                   (render-error "Bad username.")))))))
+      (render-view +error+
+                   :message "You must be logged in to create an article.")))
+
+@route app (:get "/article/:slug/changes")
+(defview changes (slug)
+  (let ((article (antimer.db:find-article slug)))
+    (if article
+        (render-view +article-changes+
+                     :changes
+                     (let ((changes (list)))
+                       (antimer.db:do-changes (change article)
+                         (push change changes))
+                       changes))
+        (render-view +error+
+                     :message "No such article."))))
+
 
 ;;; Authentication views
 
@@ -176,10 +248,16 @@
 
 (defmethod on-event ((plugin web-app) (event startup))
   "On startup, start the server."
+  (antimer.log:info :web "Starting web server on ~D" (plugin-port plugin))
+  (lucerne.app:use app
+                   (make-instance 'clack.middleware.static:<clack-middleware-static>
+                                  :root (wiki-static-directory *wiki*)
+                                  :path "/static/"))
   (start app :port (plugin-port plugin)))
 
 (defmethod on-event ((plugin web-app) (event shutdown))
   "On shutdown, stop the server."
+  (antimer.log:info :web "Shutting down web server")
   (stop app))
 
 (antimer.config:register-default-plugin
