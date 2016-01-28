79697a0f4c03360f64f77cab3c651d8383719cab
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-28 20:33:50 -0300

    Add a wiki link macro

--- src/macros.lisp
+++ src/macros.lisp
@@ -3,10 +3,15 @@
   (:use :cl)
   (:import-from :common-doc
                 :define-node
+                ;; Constructors
                 :make-content
                 :make-meta
                 :make-text
-                :reference)
+                :make-document-link
+                ;; Accessors
+                :children
+                :reference
+                :text)
   (:import-from :common-doc.macro
                 :macro-node
                 :expand-macro)
@@ -58,3 +63,37 @@
                  (common-doc:children node))
                 :reference (reference node)
                 :metadata (make-class "proof")))
+
+;;; Wiki links
+
+(define-node wiki-link (macro-node)
+  ()
+  (:tag-name "w")
+  (:documentation "A wiki link."))
+
+(defmethod expand-macro ((node wiki-link))
+  (let ((text (text (first (children node)))))
+    (destructuring-bind (url &optional title)
+        (uiop:split-string text :separator "|")
+      (destructuring-bind (document &optional section)
+          (uiop:split-string url :separator "#")
+        (make-document-link (if (uiop:emptyp document)
+                                ;; Same document
+                                nil
+                                document)
+                            ;; If the section is nil, it's just a link to the
+                            ;; document
+                            section
+                            (list
+                             (if (or (null title)
+                                     (uiop:emptyp title))
+                                 ;; The title is empty, find an article with this title
+                                 (let ((article (antimer.db:find-article document)))
+                                   (if article
+                                       (make-text
+                                        (antimer.db:article-title article))
+                                       (make-text
+                                        "[broken link]")))
+                                 ;; The title is not empty
+                                 (make-text title)))
+                            :metadata (make-class "wiki-link"))))))
