a180401f35450d423eb9eab78e5d519c5f25efce
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-04 15:27:56 -0300

    Add antimer.build

---
+++ src/build.lisp
@@ -0,0 +1,19 @@
+(in-package :cl-user)
+(defpackage antimer.build
+  (:use :cl :antimer.wiki)
+  (:documentation "Building the wiki."))
+(in-package :antimer.build)
+
+(defgeneric build (wiki)
+  (:documentation "Generate all the files necessary for the wiki.")
+
+  (:method ((wiki wiki))
+    (format t "Generating static files...")
+    ;; Generate static files through the plugins
+    (dolist (plugin (wiki-plugins wiki))
+     (when (typep plugin 'static-generator)
+       (generate wiki plugin)))
+    ;; Generate the articles
+    (format t "Generating articles")
+    (dolist (pathname (uiop:directory-files (wiki-articles-directory wiki)))
+      (format t "Compiling pathname ~A" pathname))))
