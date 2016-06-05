d3acdff7c1ea6eae88042aaaee0cacbf75e881eb
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-05 13:43:57 -0300

    Update building code

--- src/build.lisp
+++ src/build.lisp
@@ -8,6 +8,24 @@
   (:documentation "Building the wiki."))
 (in-package :antimer.build)
 
+;;; Templates
+
+(djula:add-template-directory
+ (asdf:system-relative-pathname :antimer #p"templates/"))
+
+(defparameter +index+ (djula:compile-template* "index.html"))
+
+;;; Utilities
+
+(defun render-template (wiki pathname template &rest arguments)
+  (with-open-file (stream (merge-pathnames pathname (wiki-build-directory wiki))
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+    (apply #'djula:render-template* (append (list template stream) arguments))))
+
+;;; Build
+
 (defgeneric build (wiki)
   (:documentation "Generate all the files necessary for the wiki.")
 
@@ -20,9 +38,14 @@
     ;; Delete the temporary directory
     (when (probe-file (wiki-temporary-directory wiki))
       (uiop:delete-directory-tree (wiki-temporary-directory wiki) :validate t))
-    ;; Generate the articles
-    (antimer.log:info :antimer "Compiling articles")
-    (dolist (pathname (uiop:directory-files (wiki-articles-directory wiki)))
-      (let ((article (antimer.article:parse-article wiki pathname)))
-        (print article)))
-    (values)))
+    (flet ((render (template pathname &rest arguments)
+             (apply #'render-template (append (list wiki pathname template) arguments))))
+      ;; Generate the articles
+      (antimer.log:info :antimer "Compiling articles")
+      (dolist (pathname (uiop:directory-files (wiki-articles-directory wiki)))
+        (let ((article (antimer.article:parse-article wiki pathname)))
+          (print article)))
+      ;; Create index file
+      (render +index+ #p"index.html"
+              :antimer-wiki-name (wiki-name wiki))
+      (values))))
