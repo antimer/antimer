0eaad742bbb816a5606873a4806ad078d4a5325f
Fernando Borretti <eudoxiahp@gmail.com>
2016-06-04 22:18:00 -0300

    Fix package name

--- src/standard.lisp
+++ src/standard.lisp
@@ -15,5 +15,5 @@
 (defmethod initialize-instance :after ((instance standard-wiki) &key)
   "After initializing the instance, install the plugins."
   (setf (wiki-plugins instance)
-        (append (list (make-instance 'antimer.plugins.fonts:fonts-plugin))
+        (append (list (make-instance 'antimer.plugin.fonts:fonts-plugin))
                 (wiki-plugins instance))))
